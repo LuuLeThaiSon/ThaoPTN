018baaa5eaf468855937ae2d5576602061a98a6c
LuuLeThaiSon <llts28497@gmail.com>
2025-02-20 21:04:21 +0700

    cm

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Em';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Hồng Nhung';
+const contentCard = 'Chúc em có một ngày sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau.';
+
+const giftImage = 'yêu1.jpg';
+const giftImageCard = 'yêu2.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
