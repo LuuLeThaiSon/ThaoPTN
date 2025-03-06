98836c8bb55e31ae0a470e063c8e733a3a320db6
LuuLeThaiSon <llts28497@gmail.com>
2025-03-06 19:15:33 +0700

    commit

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const nameGirl = 'PTNT';
 const giftUrl = '';
 const eventName = 'Gửi người anh thương';
 const titleCard = 'Thảo dâu';
-const contentCard = 'Chúc em có một ngày 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau. Yêu em nhiều';
+const contentCard = 'Chúc emm có một ngày 8/3 tràn ngập niềm vui và những nụ cười. Mỗi ngày được thấy emm, anh đều cảm thấy thật may mắn và hạnh phúc. Emm luôn là nguồn động lực lớn của anh. Mong điều đẹp nhất sẽ đến với emm trong hôm nay và cả những ngày sau nữa. Anh yêu emm nhiều. ';
 
 const giftImage = 'yêu1.jpg';
 const giftImageCard = 'yêu2.jpg';
